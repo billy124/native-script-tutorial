a9d7cdb9cc02bd4d3204e86563031f63a1d515de
Billy Mahmood <billy124@msn.com>
2017-09-19 20:17:43 +0100

    Completed tutorial

---
+++ platforms/ios/build/Groceries.build/Debug-iphonesimulator/Groceries.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,10 @@
+dependencies: \
+  /Users/billymahmood/Sites/mm/Groceries/platforms/ios/Groceries/Groceries-Prefix.pch \
+  /Users/billymahmood/Sites/mm/Groceries/platforms/ios/internal/main.m \
+  /Users/billymahmood/Sites/mm/Groceries/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/billymahmood/Sites/mm/Groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/billymahmood/Sites/mm/Groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/billymahmood/Sites/mm/Groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/billymahmood/Sites/mm/Groceries/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/billymahmood/Sites/mm/Groceries/platforms/ios/internal/TNSDebugging.h \
+  /Users/billymahmood/Sites/mm/Groceries/platforms/ios/internal/TKLiveSync/include/TKLiveSync.h
